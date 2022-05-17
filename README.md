6a5679dcf0c2f98e1acf1b5636e7e742881d6967
eddysantiagoo <eddysantiagogh@gmail.com>
2022-05-17 12:23:39 -0500

    Primer commit inicio del proyecto, solo se hizo una configuración de puertos

---
+++ README.md
@@ -0,0 +1,1 @@
+# Practica-stack-MERN
---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const express = require('express')
+const { appConfig } = require('./config')
+
+const app = express()
+
+app.listen(appConfig.port, () => console.log (`listen on ${appConfig.port}`))
+
