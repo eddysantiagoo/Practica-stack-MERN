fec8b0ace5813ad266a683e07ed1552c5472314d
eddysantiagoo <eddysantiagogh@gmail.com>
2022-06-04 23:54:38 -0500

    Prueba use effects

--- frontend-mern-1/src/Products/components/ListProducts.jsx
+++ frontend-mern-1/src/Products/components/ListProducts.jsx
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Loading from "./Loading";
 
 const ListProducts = () => {
     const [isLoading, setIsLoading] = useState(true);
 
+    useEffect(() => {
+
+      const timeId = setInterval(() => {
+        console.log("useEffect")
+        setIsLoading(!isLoading)
+      }, 2000)
+
+      return () => clearInterval(timeId)
+    }, []);
+
+    useEffect(() => {
+      console.log("Only once time")
+    }, [isLoading]);
+
     return (
         isLoading
         ? <Loading />
         : 'Mostar listado'
+
+
   )
 }
  
